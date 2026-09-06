437a4d59b563eb6547677740ce01b5e06b1f8232
JonathanSierks <jonathan.sierks@gmail.com>
2026-09-06 15:22:27 +0200

    deactivated heuristic training sampling

--- train.py
+++ train.py
@@ -50,7 +50,7 @@ class Config:
     beta_play: float = 0.01
     lr: float = 3e-4
     g_scale: float = 50
-    p_heur: float = 0.4                # share of heuristic training opponents
+    p_heur: float = 0                # share of heuristic training opponents
     games_per_update: int = 27
     updates: int = 50_000
     seed: int = 0
