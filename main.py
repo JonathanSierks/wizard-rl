47781fa90e7ca130f301fdc4102dcf0965c30d71
JonathanSierks <jonathan.sierks@gmail.com>
2026-09-12 14:29:35 +0200

    copied final checkpoints into the relevant_checkpoints folder, finished the analysis in experiments.ipynb, created new and updated figures, changed main.py to play against the best configuration (F)

--- main.py
+++ main.py
@@ -11,14 +11,15 @@ def start_game():
     hidden_dim = 256
 
     pro_net = WizNet(obs_dim, max_bid, hidden_dim)
-    #net_1000.load_state_dict(torch.load("relevant_checkpoints/up_20260816_160023_750.pt"))
-    #net_1000.load_state_dict(torch.load("relevant_checkpoints/up_20260815_161453_12500.pt"), strict=False)
-    pro_net.load_state_dict(torch.load("relevant_checkpoints/up_20260819_000156_12500.pt"), strict=False)
-
+    #pro_net.load_state_dict(torch.load("relevant_checkpoints/up_20260816_160023_750.pt"))
+    #pro_net.load_state_dict(torch.load("relevant_checkpoints/up_20260815_161453_12500.pt"), strict=False)
+    #pro_net.load_state_dict(torch.load("relevant_checkpoints/up_20260819_000156_12500.pt"), strict=False)
+    pro_net.load_state_dict(torch.load("relevant_checkpoints/F_seed2_20260907_190730_up2750.pt"), strict=False)
     player1 = Player("human1", 0, agent=HumanAgent())
-    #player2 = Player("rl1", 1, agent=RLAgent(pro_net, greedy=True))
-    player2 = Player("heuristic1", 1, agent=HeuristicAgent())   # explainable opponent
-    player3 = Player("heuristic2", 2, agent=HeuristicAgent())   # explainable opponent
+    player2 = Player("rl1", 1, agent=RLAgent(pro_net, greedy=True))
+    player3 = Player("rl2", 2, agent=RLAgent(pro_net, greedy=True))
+    #player2 = Player("heuristic1", 1, agent=HeuristicAgent())   # explainable opponent
+    #player3 = Player("heuristic2", 2, agent=HeuristicAgent())   # explainable opponent
 
     game = Game()
 
